795a9c5f4eeb7509c45283c8aec870dfa2039304
Ульяна <startseva.ulka@ya.ru>
2025-12-22 12:26:58 +0500

    ллддлдл

--- Разработка анимаций мобильной версии/main.js
+++
@@ -1,6 +0,0 @@
-const header = document.querySelector(".header");
-const burger = document.querySelector(".burger");
-
-burger.addEventListener("click", () => {
-  header.classList.toggle("header--open");
-})
